f6aa5bc62bf75255f700824c548eeb6988a712c4
Keith Brewster <keith.brewster@cbc.ca>
2020-06-11 12:15:53 -0400

    move app logic to main.js and use index for launching point + enable esm

--- index.js
+++ index.js
@@ -1,41 +1,4 @@
-const chalk = require("chalk");
-const clear = require("clear");
-const figlet = require("figlet");
+#!/usr/bin/env node
 
-const cli = require("./lib/cli");
-const commit = require("./lib/commit");
-
-clear();
-
-console.log(
-  chalk.magenta(
-    figlet.textSync("tagged-commits", {
-      font: "slant",
-    }),
-    "\n"
-  )
-);
-
-/**
- * Start the application
- *
- * @returns {void}
- */
-const run = async () => {
-  try {
-    const commitData = await cli.getCommitInfo();
-    const commitMessage = commit.buildCommitMessage(commitData);
-
-    console.log(chalk.green(`\n ${commitMessage} \n`));
-
-    const { confirmed } = await cli.confirmCommit();
-
-    if (confirmed) {
-      commit.writeCommit(commitMessage);
-    }
-  } catch (e) {
-    throw e;
-  }
-};
-
-run();
+require = require("esm")(module /*, options*/);
+module.exports = require("./main.js");
